ade7125ca691050cd5ec6fa365b830563b613da4
Shadow-Noxxx <jangraji518@gmail.com>
2025-06-13 23:23:01 +0530

    Update rss_sql.py

--- tg_bot/modules/sql/rss_sql.py
+++ tg_bot/modules/sql/rss_sql.py
@@ -1,4 +1,5 @@
 import threading
+from typing import List
 
 from sqlalchemy import Column, UnicodeText, Integer
 
@@ -7,72 +8,63 @@ from tg_bot.modules.sql import BASE, SESSION
 
 class RSS(BASE):
     __tablename__ = "rss_feed"
+
     id = Column(Integer, primary_key=True)
     chat_id = Column(UnicodeText, nullable=False)
     feed_link = Column(UnicodeText)
     old_entry_link = Column(UnicodeText)
 
-    def __init__(self, chat_id, feed_link, old_entry_link):
+    def __init__(self, chat_id: str, feed_link: str, old_entry_link: str):
         self.chat_id = chat_id
         self.feed_link = feed_link
         self.old_entry_link = old_entry_link
 
     def __repr__(self):
-        return "<RSS for chatID {} at feed_link {} with old_entry_link {}>".format(self.chat_id,
-                                                                                   self.feed_link,
-                                                                                   self.old_entry_link)
+        return f"<RSS chat_id={self.chat_id} feed={self.feed_link} old={self.old_entry_link}>"
 
 
-RSS.__table__.create(checkfirst=True)
 INSERTION_LOCK = threading.RLock()
 
 
-def check_url_availability(tg_chat_id, tg_feed_link):
+def check_url_availability(chat_id: str, feed_link: str) -> List[RSS]:
     try:
-        return SESSION.query(RSS).filter(RSS.feed_link == tg_feed_link,
-                                         RSS.chat_id == tg_chat_id).all()
+        return SESSION.query(RSS).filter_by(chat_id=chat_id, feed_link=feed_link).all()
     finally:
         SESSION.close()
 
 
-def add_url(tg_chat_id, tg_feed_link, tg_old_entry_link):
+def add_url(chat_id: str, feed_link: str, old_entry_link: str):
     with INSERTION_LOCK:
-        action = RSS(tg_chat_id, tg_feed_link, tg_old_entry_link)
-
-        SESSION.add(action)
+        entry = RSS(chat_id, feed_link, old_entry_link)
+        SESSION.add(entry)
         SESSION.commit()
 
 
-def remove_url(tg_chat_id, tg_feed_link):
+def remove_url(chat_id: str, feed_link: str):
     with INSERTION_LOCK:
-        # this loops to delete any possible duplicates for the same TG User ID, TG Chat ID and link
-        for row in check_url_availability(tg_chat_id, tg_feed_link):
-            # add the action to the DB query
+        rows = check_url_availability(chat_id, feed_link)
+        for row in rows:
             SESSION.delete(row)
-
         SESSION.commit()
 
 
-def get_urls(tg_chat_id):
+def get_urls(chat_id: str) -> List[RSS]:
     try:
-        return SESSION.query(RSS).filter(RSS.chat_id == tg_chat_id).all()
+        return SESSION.query(RSS).filter_by(chat_id=chat_id).all()
     finally:
         SESSION.close()
 
 
-def get_all():
+def get_all() -> List[RSS]:
     try:
         return SESSION.query(RSS).all()
     finally:
         SESSION.close()
 
 
-def update_url(row_id, new_entry_links):
+def update_url(row_id: int, new_entry_link: str):
     with INSERTION_LOCK:
         row = SESSION.query(RSS).get(row_id)
-
-        # set the new old_entry_link with the latest update from the RSS Feed
-        row.old_entry_link = new_entry_links[0]
-
-        # commit the changes to the DB
-        SESSION.commit()
+        if row:
+            row.old_entry_link = new_entry_link
+            SESSION.commit()
