1162ddd1d86a1839542f63b0567c227094453cbf
Shadow-Noxxx <jangraji518@gmail.com>
2025-06-13 23:27:32 +0530

    Update warns_sql.py

--- tg_bot/modules/sql/warns_sql.py
+++ tg_bot/modules/sql/warns_sql.py
@@ -1,4 +1,5 @@
 import threading
+from typing import Optional, Tuple, List
 
 from sqlalchemy import Integer, Column, String, UnicodeText, func, distinct, Boolean
 from sqlalchemy.dialects import postgresql
@@ -14,273 +15,242 @@ class Warns(BASE):
     num_warns = Column(Integer, default=0)
     reasons = Column(postgresql.ARRAY(UnicodeText))
 
-    def __init__(self, user_id, chat_id):
+    def __init__(self, user_id: int, chat_id: str):
         self.user_id = user_id
         self.chat_id = str(chat_id)
         self.num_warns = 0
         self.reasons = []
 
     def __repr__(self):
-        return "<{} warns for {} in {} for reasons {}>".format(self.num_warns, self.user_id, self.chat_id, self.reasons)
+        return f"<{self.num_warns} warns for {self.user_id} in {self.chat_id} for reasons {self.reasons}>"
 
 
 class WarnFilters(BASE):
     __tablename__ = "warn_filters"
+
     chat_id = Column(String(14), primary_key=True)
     keyword = Column(UnicodeText, primary_key=True, nullable=False)
     reply = Column(UnicodeText, nullable=False)
 
-    def __init__(self, chat_id, keyword, reply):
-        self.chat_id = str(chat_id)  # ensure string
+    def __init__(self, chat_id: str, keyword: str, reply: str):
+        self.chat_id = str(chat_id)
         self.keyword = keyword
         self.reply = reply
 
     def __repr__(self):
-        return "<Permissions for %s>" % self.chat_id
+        return f"<WarnFilter {self.keyword} for chat {self.chat_id}>"
 
     def __eq__(self, other):
-        return bool(isinstance(other, WarnFilters)
-                    and self.chat_id == other.chat_id
-                    and self.keyword == other.keyword)
+        return isinstance(other, WarnFilters) and self.chat_id == other.chat_id and self.keyword == other.keyword
 
 
 class WarnSettings(BASE):
     __tablename__ = "warn_settings"
+
     chat_id = Column(String(14), primary_key=True)
     warn_limit = Column(Integer, default=3)
     soft_warn = Column(Boolean, default=False)
 
-    def __init__(self, chat_id, warn_limit=3, soft_warn=False):
+    def __init__(self, chat_id: str, warn_limit: int = 3, soft_warn: bool = False):
         self.chat_id = str(chat_id)
         self.warn_limit = warn_limit
         self.soft_warn = soft_warn
 
     def __repr__(self):
-        return "<{} has {} possible warns.>".format(self.chat_id, self.warn_limit)
+        return f"<{self.chat_id} has warn limit {self.warn_limit}>"
 
 
-Warns.__table__.create(checkfirst=True)
-WarnFilters.__table__.create(checkfirst=True)
-WarnSettings.__table__.create(checkfirst=True)
-
+# Locks
 WARN_INSERTION_LOCK = threading.RLock()
 WARN_FILTER_INSERTION_LOCK = threading.RLock()
 WARN_SETTINGS_LOCK = threading.RLock()
 
-WARN_FILTERS = {}
+# Cache for fast access
+WARN_FILTERS: dict[str, List[str]] = {}
 
 
-def warn_user(user_id, chat_id, reason=None):
+# Warn Management
+def warn_user(user_id: int, chat_id: str, reason: Optional[str] = None) -> Tuple[int, List[str]]:
     with WARN_INSERTION_LOCK:
-        warned_user = SESSION.query(Warns).get((user_id, str(chat_id)))
-        if not warned_user:
-            warned_user = Warns(user_id, str(chat_id))
-
+        warned_user = SESSION.query(Warns).get((user_id, str(chat_id))) or Warns(user_id, chat_id)
         warned_user.num_warns += 1
         if reason:
-            warned_user.reasons = warned_user.reasons + [reason]  # TODO:: double check this wizardry
-
-        reasons = warned_user.reasons
-        num = warned_user.num_warns
+            warned_user.reasons = (warned_user.reasons or []) + [reason]
 
         SESSION.add(warned_user)
         SESSION.commit()
+        return warned_user.num_warns, warned_user.reasons
 
-        return num, reasons
 
-
-def remove_warn(user_id, chat_id):
+def remove_warn(user_id: int, chat_id: str) -> bool:
     with WARN_INSERTION_LOCK:
-        removed = False
         warned_user = SESSION.query(Warns).get((user_id, str(chat_id)))
-
         if warned_user and warned_user.num_warns > 0:
             warned_user.num_warns -= 1
-
             SESSION.add(warned_user)
             SESSION.commit()
-            removed = True
-
-        SESSION.close()
-        return removed
+            return True
+        return False
 
 
-def reset_warns(user_id, chat_id):
+def reset_warns(user_id: int, chat_id: str):
     with WARN_INSERTION_LOCK:
         warned_user = SESSION.query(Warns).get((user_id, str(chat_id)))
         if warned_user:
             warned_user.num_warns = 0
             warned_user.reasons = []
-
             SESSION.add(warned_user)
             SESSION.commit()
-        SESSION.close()
 
 
-def get_warns(user_id, chat_id):
+def get_warns(user_id: int, chat_id: str) -> Optional[Tuple[int, List[str]]]:
     try:
         user = SESSION.query(Warns).get((user_id, str(chat_id)))
         if not user:
             return None
-        reasons = user.reasons
-        num = user.num_warns
-        return num, reasons
+        return user.num_warns, user.reasons
     finally:
         SESSION.close()
 
 
-def add_warn_filter(chat_id, keyword, reply):
+# Warn Filters
+def add_warn_filter(chat_id: str, keyword: str, reply: str):
     with WARN_FILTER_INSERTION_LOCK:
-        warn_filt = WarnFilters(str(chat_id), keyword, reply)
+        warn_filt = WarnFilters(chat_id, keyword, reply)
+        if keyword not in WARN_FILTERS.get(chat_id, []):
+            WARN_FILTERS.setdefault(chat_id, []).append(keyword)
+            WARN_FILTERS[chat_id] = sorted(WARN_FILTERS[chat_id], key=lambda x: (-len(x), x))
 
-        if keyword not in WARN_FILTERS.get(str(chat_id), []):
-            WARN_FILTERS[str(chat_id)] = sorted(WARN_FILTERS.get(str(chat_id), []) + [keyword],
-                                                key=lambda x: (-len(x), x))
-
-        SESSION.merge(warn_filt)  # merge to avoid duplicate key issues
+        SESSION.merge(warn_filt)
         SESSION.commit()
 
 
-def remove_warn_filter(chat_id, keyword):
+def remove_warn_filter(chat_id: str, keyword: str) -> bool:
     with WARN_FILTER_INSERTION_LOCK:
-        warn_filt = SESSION.query(WarnFilters).get((str(chat_id), keyword))
+        warn_filt = SESSION.query(WarnFilters).get((chat_id, keyword))
         if warn_filt:
-            if keyword in WARN_FILTERS.get(str(chat_id), []):  # sanity check
-                WARN_FILTERS.get(str(chat_id), []).remove(keyword)
-
+            if keyword in WARN_FILTERS.get(chat_id, []):
+                WARN_FILTERS[chat_id].remove(keyword)
             SESSION.delete(warn_filt)
             SESSION.commit()
             return True
-        SESSION.close()
         return False
 
 
-def get_chat_warn_triggers(chat_id):
-    return WARN_FILTERS.get(str(chat_id), set())
+def get_chat_warn_triggers(chat_id: str) -> List[str]:
+    return WARN_FILTERS.get(chat_id, [])
 
 
-def get_chat_warn_filters(chat_id):
+def get_chat_warn_filters(chat_id: str) -> List[WarnFilters]:
     try:
-        return SESSION.query(WarnFilters).filter(WarnFilters.chat_id == str(chat_id)).all()
+        return SESSION.query(WarnFilters).filter_by(chat_id=chat_id).all()
     finally:
         SESSION.close()
 
 
-def get_warn_filter(chat_id, keyword):
+def get_warn_filter(chat_id: str, keyword: str) -> Optional[WarnFilters]:
     try:
-        return SESSION.query(WarnFilters).get((str(chat_id), keyword))
+        return SESSION.query(WarnFilters).get((chat_id, keyword))
     finally:
         SESSION.close()
 
 
-def set_warn_limit(chat_id, warn_limit):
+# Settings
+def set_warn_limit(chat_id: str, warn_limit: int):
     with WARN_SETTINGS_LOCK:
-        curr_setting = SESSION.query(WarnSettings).get(str(chat_id))
-        if not curr_setting:
-            curr_setting = WarnSettings(chat_id, warn_limit=warn_limit)
-
-        curr_setting.warn_limit = warn_limit
-
-        SESSION.add(curr_setting)
+        setting = SESSION.query(WarnSettings).get(chat_id) or WarnSettings(chat_id, warn_limit=warn_limit)
+        setting.warn_limit = warn_limit
+        SESSION.add(setting)
         SESSION.commit()
 
 
-def set_warn_strength(chat_id, soft_warn):
+def set_warn_strength(chat_id: str, soft_warn: bool):
     with WARN_SETTINGS_LOCK:
-        curr_setting = SESSION.query(WarnSettings).get(str(chat_id))
-        if not curr_setting:
-            curr_setting = WarnSettings(chat_id, soft_warn=soft_warn)
-
-        curr_setting.soft_warn = soft_warn
-
-        SESSION.add(curr_setting)
+        setting = SESSION.query(WarnSettings).get(chat_id) or WarnSettings(chat_id, soft_warn=soft_warn)
+        setting.soft_warn = soft_warn
+        SESSION.add(setting)
         SESSION.commit()
 
 
-def get_warn_setting(chat_id):
+def get_warn_setting(chat_id: str) -> Tuple[int, bool]:
     try:
-        setting = SESSION.query(WarnSettings).get(str(chat_id))
-        if setting:
-            return setting.warn_limit, setting.soft_warn
-        else:
-            return 3, False
-
+        setting = SESSION.query(WarnSettings).get(chat_id)
+        return (setting.warn_limit, setting.soft_warn) if setting else (3, False)
     finally:
         SESSION.close()
 
 
-def num_warns():
+# Stats
+def num_warns() -> int:
     try:
         return SESSION.query(func.sum(Warns.num_warns)).scalar() or 0
     finally:
         SESSION.close()
 
 
-def num_warn_chats():
+def num_warn_chats() -> int:
     try:
         return SESSION.query(func.count(distinct(Warns.chat_id))).scalar()
     finally:
         SESSION.close()
 
 
-def num_warn_filters():
+def num_warn_filters() -> int:
     try:
         return SESSION.query(WarnFilters).count()
     finally:
         SESSION.close()
 
 
-def num_warn_chat_filters(chat_id):
+def num_warn_chat_filters(chat_id: str) -> int:
     try:
-        return SESSION.query(WarnFilters.chat_id).filter(WarnFilters.chat_id == str(chat_id)).count()
+        return SESSION.query(WarnFilters).filter_by(chat_id=chat_id).count()
     finally:
         SESSION.close()
 
 
-def num_warn_filter_chats():
+def num_warn_filter_chats() -> int:
     try:
         return SESSION.query(func.count(distinct(WarnFilters.chat_id))).scalar()
     finally:
         SESSION.close()
 
 
+# Migrations
+def migrate_chat(old_chat_id: str, new_chat_id: str):
+    with WARN_INSERTION_LOCK:
+        for warn in SESSION.query(Warns).filter_by(chat_id=old_chat_id).all():
+            warn.chat_id = new_chat_id
+        SESSION.commit()
+
+    with WARN_FILTER_INSERTION_LOCK:
+        for filt in SESSION.query(WarnFilters).filter_by(chat_id=old_chat_id).all():
+            filt.chat_id = new_chat_id
+        SESSION.commit()
+        if old_chat_id in WARN_FILTERS:
+            WARN_FILTERS[new_chat_id] = WARN_FILTERS.pop(old_chat_id)
+
+    with WARN_SETTINGS_LOCK:
+        for setting in SESSION.query(WarnSettings).filter_by(chat_id=old_chat_id).all():
+            setting.chat_id = new_chat_id
+        SESSION.commit()
+
+
+# Initial Load
 def __load_chat_warn_filters():
     global WARN_FILTERS
     try:
         chats = SESSION.query(WarnFilters.chat_id).distinct().all()
-        for (chat_id,) in chats:  # remove tuple by ( ,)
+        for (chat_id,) in chats:
             WARN_FILTERS[chat_id] = []
 
-        all_filters = SESSION.query(WarnFilters).all()
-        for x in all_filters:
-            WARN_FILTERS[x.chat_id] += [x.keyword]
+        for filt in SESSION.query(WarnFilters).all():
+            WARN_FILTERS[filt.chat_id].append(filt.keyword)
 
-        WARN_FILTERS = {x: sorted(set(y), key=lambda i: (-len(i), i)) for x, y in WARN_FILTERS.items()}
-
+        for cid in WARN_FILTERS:
+            WARN_FILTERS[cid] = sorted(set(WARN_FILTERS[cid]), key=lambda i: (-len(i), i))
     finally:
         SESSION.close()
-
-
-def migrate_chat(old_chat_id, new_chat_id):
-    with WARN_INSERTION_LOCK:
-        chat_notes = SESSION.query(Warns).filter(Warns.chat_id == str(old_chat_id)).all()
-        for note in chat_notes:
-            note.chat_id = str(new_chat_id)
-        SESSION.commit()
-
-    with WARN_FILTER_INSERTION_LOCK:
-        chat_filters = SESSION.query(WarnFilters).filter(WarnFilters.chat_id == str(old_chat_id)).all()
-        for filt in chat_filters:
-            filt.chat_id = str(new_chat_id)
-        SESSION.commit()
-        WARN_FILTERS[str(new_chat_id)] = WARN_FILTERS[str(old_chat_id)]
-        del WARN_FILTERS[str(old_chat_id)]
-
-    with WARN_SETTINGS_LOCK:
-        chat_settings = SESSION.query(WarnSettings).filter(WarnSettings.chat_id == str(old_chat_id)).all()
-        for setting in chat_settings:
-            setting.chat_id = str(new_chat_id)
-        SESSION.commit()
 
 
 __load_chat_warn_filters()
