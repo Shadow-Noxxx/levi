d4b9388d70c3be8cf2bd2febdd1f8dd31a1001c8
Shadow-Noxxx <jangraji518@gmail.com>
2025-06-13 23:19:53 +0530

    Update locks_sql.py

--- tg_bot/modules/sql/locks_sql.py
+++ tg_bot/modules/sql/locks_sql.py
@@ -1,15 +1,11 @@
-# New chat added -> setup permissions
 import threading
-
 from sqlalchemy import Column, String, Boolean
-
 from tg_bot.modules.sql import SESSION, BASE
-
 
 class Permissions(BASE):
     __tablename__ = "permissions"
     chat_id = Column(String(14), primary_key=True)
-    # Booleans are for "is this locked", _NOT_ "is this allowed"
+    # Booleans mean "is locked"
     audio = Column(Boolean, default=False)
     voice = Column(Boolean, default=False)
     contact = Column(Boolean, default=False)
@@ -26,48 +22,19 @@ class Permissions(BASE):
     location = Column(Boolean, default=False)
 
     def __init__(self, chat_id):
-        self.chat_id = str(chat_id)  # ensure string
-        self.audio = False
-        self.voice = False
-        self.contact = False
-        self.video = False
-        self.videonote = False
-        self.document = False
-        self.photo = False
-        self.sticker = False
-        self.gif = False
-        self.url = False
-        self.bots = False
-        self.forward = False
-        self.game = False
-        self.location = False
-
-    def __repr__(self):
-        return "<Permissions for %s>" % self.chat_id
+        self.chat_id = str(chat_id)
 
 
 class Restrictions(BASE):
     __tablename__ = "restrictions"
     chat_id = Column(String(14), primary_key=True)
-    # Booleans are for "is this restricted", _NOT_ "is this allowed"
     messages = Column(Boolean, default=False)
     media = Column(Boolean, default=False)
     other = Column(Boolean, default=False)
     preview = Column(Boolean, default=False)
 
     def __init__(self, chat_id):
-        self.chat_id = str(chat_id)  # ensure string
-        self.messages = False
-        self.media = False
-        self.other = False
-        self.preview = False
-
-    def __repr__(self):
-        return "<Restrictions for %s>" % self.chat_id
-
-
-Permissions.__table__.create(checkfirst=True)
-Restrictions.__table__.create(checkfirst=True)
+        self.chat_id = str(chat_id)
 
 
 PERM_LOCK = threading.RLock()
@@ -75,143 +42,71 @@ RESTR_LOCK = threading.RLock()
 
 
 def init_permissions(chat_id, reset=False):
-    curr_perm = SESSION.query(Permissions).get(str(chat_id))
-    if reset:
-        SESSION.delete(curr_perm)
+    curr = SESSION.query(Permissions).get(str(chat_id))
+    if reset and curr:
+        SESSION.delete(curr)
         SESSION.flush()
-    perm = Permissions(str(chat_id))
-    SESSION.add(perm)
+    new_perm = Permissions(chat_id)
+    SESSION.add(new_perm)
     SESSION.commit()
-    return perm
+    return new_perm
 
 
 def init_restrictions(chat_id, reset=False):
-    curr_restr = SESSION.query(Restrictions).get(str(chat_id))
-    if reset:
-        SESSION.delete(curr_restr)
+    curr = SESSION.query(Restrictions).get(str(chat_id))
+    if reset and curr:
+        SESSION.delete(curr)
         SESSION.flush()
-    restr = Restrictions(str(chat_id))
-    SESSION.add(restr)
+    new_restr = Restrictions(chat_id)
+    SESSION.add(new_restr)
     SESSION.commit()
-    return restr
+    return new_restr
 
 
 def update_lock(chat_id, lock_type, locked):
     with PERM_LOCK:
-        curr_perm = SESSION.query(Permissions).get(str(chat_id))
-        if not curr_perm:
-            curr_perm = init_permissions(chat_id)
-
-        if lock_type == "audio":
-            curr_perm.audio = locked
-        elif lock_type == "voice":
-            curr_perm.voice = locked
-        elif lock_type == "contact":
-            curr_perm.contact = locked
-        elif lock_type == "video":
-            curr_perm.video = locked
-        elif lock_type == "videonote":
-            curr_perm.videonote = locked
-        elif lock_type == "document":
-            curr_perm.document = locked
-        elif lock_type == "photo":
-            curr_perm.photo = locked
-        elif lock_type == "sticker":
-            curr_perm.sticker = locked
-        elif lock_type == "gif":
-            curr_perm.gif = locked
-        elif lock_type == 'url':
-            curr_perm.url = locked
-        elif lock_type == 'bots':
-            curr_perm.bots = locked
-        elif lock_type == 'forward':
-            curr_perm.forward = locked
-        elif lock_type == 'game':
-            curr_perm.game = locked
-        elif lock_type == 'location':
-            curr_perm.location = locked
-
-        SESSION.add(curr_perm)
+        curr = SESSION.query(Permissions).get(str(chat_id))
+        if not curr:
+            curr = init_permissions(chat_id)
+        setattr(curr, lock_type, locked)
         SESSION.commit()
 
 
 def update_restriction(chat_id, restr_type, locked):
     with RESTR_LOCK:
-        curr_restr = SESSION.query(Restrictions).get(str(chat_id))
-        if not curr_restr:
-            curr_restr = init_restrictions(chat_id)
+        curr = SESSION.query(Restrictions).get(str(chat_id))
+        if not curr:
+            curr = init_restrictions(chat_id)
 
-        if restr_type == "messages":
-            curr_restr.messages = locked
-        elif restr_type == "media":
-            curr_restr.media = locked
-        elif restr_type == "other":
-            curr_restr.other = locked
-        elif restr_type == "previews":
-            curr_restr.preview = locked
-        elif restr_type == "all":
-            curr_restr.messages = locked
-            curr_restr.media = locked
-            curr_restr.other = locked
-            curr_restr.preview = locked
-        SESSION.add(curr_restr)
+        if restr_type == "all":
+            curr.messages = curr.media = curr.other = curr.preview = locked
+        else:
+            if restr_type == "previews":
+                restr_type = "preview"
+            setattr(curr, restr_type, locked)
+
         SESSION.commit()
 
 
 def is_locked(chat_id, lock_type):
-    curr_perm = SESSION.query(Permissions).get(str(chat_id))
+    curr = SESSION.query(Permissions).get(str(chat_id))
+    result = getattr(curr, lock_type, False) if curr else False
     SESSION.close()
-
-    if not curr_perm:
-        return False
-
-    elif lock_type == "sticker":
-        return curr_perm.sticker
-    elif lock_type == "photo":
-        return curr_perm.photo
-    elif lock_type == "audio":
-        return curr_perm.audio
-    elif lock_type == "voice":
-        return curr_perm.voice
-    elif lock_type == "contact":
-        return curr_perm.contact
-    elif lock_type == "video":
-        return curr_perm.video
-    elif lock_type == "videonote":
-        return curr_perm.videonote
-    elif lock_type == "document":
-        return curr_perm.document
-    elif lock_type == "gif":
-        return curr_perm.gif
-    elif lock_type == "url":
-        return curr_perm.url
-    elif lock_type == "bots":
-        return curr_perm.bots
-    elif lock_type == "forward":
-        return curr_perm.forward
-    elif lock_type == "game":
-        return curr_perm.game
-    elif lock_type == "location":
-        return curr_perm.location
+    return result
 
 
 def is_restr_locked(chat_id, lock_type):
-    curr_restr = SESSION.query(Restrictions).get(str(chat_id))
+    curr = SESSION.query(Restrictions).get(str(chat_id))
+    result = False
+    if curr:
+        if lock_type == "all":
+            result = curr.messages and curr.media and curr.other and curr.preview
+        elif lock_type == "previews":
+            result = curr.preview
+        else:
+            result = getattr(curr, lock_type, False)
     SESSION.close()
-
-    if not curr_restr:
-        return False
-
-    if lock_type == "messages":
-        return curr_restr.messages
-    elif lock_type == "media":
-        return curr_restr.media
-    elif lock_type == "other":
-        return curr_restr.other
-    elif lock_type == "previews":
-        return curr_restr.preview
-    elif lock_type == "all":
-        return curr_restr.messages and curr_restr.media and curr_restr.other and curr_restr.preview
+    return result
 
 
 def get_locks(chat_id):
