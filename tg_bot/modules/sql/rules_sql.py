322c2ea326aa8298c1d6b1fee25d96e804c51de2
Shadow-Noxxx <jangraji518@gmail.com>
2025-06-13 23:23:38 +0530

    Update rules_sql.py

--- tg_bot/modules/sql/rules_sql.py
+++ tg_bot/modules/sql/rules_sql.py
@@ -1,4 +1,5 @@
 import threading
+from typing import Optional, Union
 
 from sqlalchemy import Column, String, UnicodeText, func, distinct
 
@@ -7,52 +8,54 @@ from tg_bot.modules.sql import SESSION, BASE
 
 class Rules(BASE):
     __tablename__ = "rules"
+
     chat_id = Column(String(14), primary_key=True)
     rules = Column(UnicodeText, default="")
 
-    def __init__(self, chat_id):
-        self.chat_id = chat_id
+    def __init__(self, chat_id: Union[int, str]):
+        self.chat_id = str(chat_id)
 
     def __repr__(self):
-        return "<Chat {} rules: {}>".format(self.chat_id, self.rules)
+        return f"<Rules chat_id={self.chat_id} rules={self.rules[:30]}...>"
 
-
-Rules.__table__.create(checkfirst=True)
 
 INSERTION_LOCK = threading.RLock()
 
 
-def set_rules(chat_id, rules_text):
+def set_rules(chat_id: Union[int, str], rules_text: str):
     with INSERTION_LOCK:
-        rules = SESSION.query(Rules).get(str(chat_id))
-        if not rules:
-            rules = Rules(str(chat_id))
-        rules.rules = rules_text
+        chat_id = str(chat_id)
+        record = SESSION.query(Rules).get(chat_id)
+        if not record:
+            record = Rules(chat_id)
 
-        SESSION.add(rules)
+        record.rules = rules_text
+        SESSION.add(record)
         SESSION.commit()
 
 
-def get_rules(chat_id):
-    rules = SESSION.query(Rules).get(str(chat_id))
-    ret = ""
-    if rules:
-        ret = rules.rules
+def get_rules(chat_id: Union[int, str]) -> Optional[str]:
+    try:
+        chat_id = str(chat_id)
+        record = SESSION.query(Rules).get(chat_id)
+        return record.rules if record else None
+    finally:
+        SESSION.close()
 
-    SESSION.close()
-    return ret
 
-
-def num_chats():
+def num_chats() -> int:
     try:
         return SESSION.query(func.count(distinct(Rules.chat_id))).scalar()
     finally:
         SESSION.close()
 
 
-def migrate_chat(old_chat_id, new_chat_id):
+def migrate_chat(old_chat_id: Union[int, str], new_chat_id: Union[int, str]):
     with INSERTION_LOCK:
-        chat = SESSION.query(Rules).get(str(old_chat_id))
-        if chat:
-            chat.chat_id = str(new_chat_id)
-        SESSION.commit()
+        record = SESSION.query(Rules).get(str(old_chat_id))
+        if record:
+            # Avoid PK conflict if new_chat_id already exists
+            SESSION.query(Rules).filter_by(chat_id=str(new_chat_id)).delete()
+
+            record.chat_id = str(new_chat_id)
+            SESSION.commit()
