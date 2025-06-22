d8aa3814a43873255ae3e3dc43781e5e4037af64
Shadow-Noxxx <jangraji518@gmail.com>
2025-06-22 21:31:27 +0530

    Update config.py

--- tg_bot/config.py
+++ tg_bot/config.py
@@ -3,7 +3,7 @@ from tg_bot.sample_config import Config
 class Development(Config):
     OWNER_ID = 7819315360  # Your Telegram user ID
     OWNER_USERNAME = "@fos_founder"  # Your @username
-    API_KEY = "7252339580:AAGz8jW4iqSrOM6NQ_ybIjckzaVqbMYCM1A"
+    API_KEY = "8013665655:AAFweEVPS0k5lXdv3Axn6KM-Hfub74rmkh8"
     SQLALCHEMY_DATABASE_URI = 'mongodb+srv://TEAMBABY01:UTTAMRATHORE09@cluster0.vmjl9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
     MESSAGE_DUMP = '-1002620872464'  # Optional: a private group/channel ID to log deleted messages
     USE_MESSAGE_DUMP = True
